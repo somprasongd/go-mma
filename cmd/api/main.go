7ddb91b9111d42205acc9517bbd07a8017f38761
Somprasong Damyos <somprasong.damyos@gmail.com>
2025-01-30 17:32:43 +0700

    feat: handle graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-mma/config"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
-)
-
-const (
-	port = 8080
 )
 
 func main() {
@@ -18,11 +19,34 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
+		time.Sleep(10 * time.Second)
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello World",
 		})
 	})
-	r.Run(fmt.Sprintf(":%d", config.HTTPPort))
+
+	httpServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", config.HTTPPort),
+		Handler: r,
+	}
+	go func() {
+		log.Printf("Starting server on port %d", config.HTTPPort)
+		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	// Handle graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+	log.Println("Shutting down server")
+	if err := httpServer.Shutdown(context.Background()); err != nil {
+		log.Printf("Server shutdown error: %v", err)
+	}
+	log.Println("Server stopped")
 }
