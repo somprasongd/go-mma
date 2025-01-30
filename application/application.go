d409e7fe3c43bf8853b53576de2d41ae4ad5425b
Somprasong Damyos <somprasong.damyos@gmail.com>
2025-01-30 17:51:35 +0700

    feat: move code to application.go

---
+++ application/application.go
@@ -0,0 +1,71 @@
+package application
+
+import (
+	"context"
+	"fmt"
+	"go-mma/config"
+	"log"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type Application struct {
+	config config.Config
+
+	httpRouter *gin.Engine
+	httpServer *http.Server
+}
+
+func New(config config.Config) *Application {
+	httpRouter := gin.Default()
+	return &Application{
+		config:     config,
+		httpRouter: httpRouter,
+		httpServer: &http.Server{
+			Addr:    fmt.Sprintf(":%d", config.HTTPPort),
+			Handler: httpRouter,
+		},
+	}
+}
+
+func (app *Application) Run() error {
+	app.startHTTPServer()
+
+	return nil
+}
+
+func (app *Application) startHTTPServer() {
+	go func() {
+		log.Printf("Starting server on port %d", app.config.HTTPPort)
+		if err := app.httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+}
+
+func (app *Application) RegisterRoutes() {
+	// global middleware
+	app.httpRouter.Use(gin.Logger())
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	app.httpRouter.Use(gin.Recovery())
+	app.httpRouter.Use(cors.Default()) // allows all origins
+
+	// route handler
+	app.httpRouter.GET("/", func(c *gin.Context) {
+		// time.Sleep(10 * time.Second)
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+}
+
+func (app *Application) Shutdown() error {
+	log.Println("Shutting down server")
+	if err := app.httpServer.Shutdown(context.Background()); err != nil {
+		log.Printf("Server shutdown error: %v", err)
+	}
+	log.Println("Server stopped")
+	return nil
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"go-mma/application"
 	"go-mma/config"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -20,33 +15,13 @@ func main() {
 		log.Panic(err)
 	}
 
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		time.Sleep(10 * time.Second)
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
-		})
-	})
-
-	httpServer := &http.Server{
-		Addr:    fmt.Sprintf(":%d", config.HTTPPort),
-		Handler: r,
-	}
-	go func() {
-		log.Printf("Starting server on port %d", config.HTTPPort)
-		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatalf("Error starting server: %v", err)
-		}
-	}()
-
+	app := application.New(*config)
+	app.RegisterRoutes()
+	app.Run()
 	// Handle graceful shutdown
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
 	<-stop
-	log.Println("Shutting down server")
-	if err := httpServer.Shutdown(context.Background()); err != nil {
-		log.Printf("Server shutdown error: %v", err)
-	}
-	log.Println("Server stopped")
+	app.Shutdown()
 }
